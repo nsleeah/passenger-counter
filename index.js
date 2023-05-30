6b0c52f345d8c6ca4557b297bdca37a37675e542
Leahnicoles <lnsimonn@hotmail.com>
2023-05-30 12:17:40 +0100

    add button for incrementing count

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-document.getElementById("count").innerText = 5
 
+//DOM Variables
+let countEl = document.getElementById("count-el")
+let count = 0;
+
+let increment = () => {
+    count = count += 1;
+    countEl.innerText = count;
+}
+
+
+// node index.js
 
