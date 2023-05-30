0b92dbb535314b256ead3e015896007be804df0d
Leahnicoles <lnsimonn@hotmail.com>
2023-05-30 13:04:18 +0100

    add save btn and implement save function

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 
 //DOM Variables
 let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
 let count = 0;
 
 let increment = () => {
     count = count += 1;
     countEl.innerText = count;
+}
+
+let save = () => {
+    let countStr = count + " - ";
+    saveEl.textContent += countStr
+    //RESET.
+    countEl.textContent = 0
+    count = 0
 }
 
 
